ee7357e9eccd90b518bbdc95ca634e46b9167ea1
Yahampath Chandika <yhmpth@gmail.com>
2024-11-08 22:44:47 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+// src/App.jsx
+function App() {
+  return (
+    <div className="bg-soft-pink text-dark-pink p-8 rounded-lg">
+      <h1 className="text-3xl font-bold text-rose-pink">Welcome to My Pink Themed Project!</h1>
+      <p className="text-blush-white">This is a soft white text.</p>
+    </div>
+  );
+}
+
+export default App;
