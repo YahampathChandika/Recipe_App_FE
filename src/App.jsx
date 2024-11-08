2519741552bdb6d11613cb8b863763efe93cf88d
Yahampath Chandika <yhmpth@gmail.com>
2024-11-09 00:47:02 +0530

    Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import RegisterUser from "./pages/RegisterUser";
+import Home from "./pages/Home";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/registerUser" element={<RegisterUser />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </Router>
   );
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import logo from "../assets/images/logo.png";
+import { IconButton } from "@mui/material";
+
+export default function Navbar() {
+  const navigate = useNavigate();
+
+  return (
+    <nav className="flex items-center justify-between bg-white-snow px-8 md:px-32 py-4 shadow-md w-full">
+      <div className="flex items-center">
+        <img src={logo} alt="Logo" className="w-16 md:w-24" />
+      </div>
+      <div className="flex space-x-5 md:space-x-24">
+        <button
+          onClick={() => navigate("/home")}
+          className="text-dark-pink transition-all duration-300 hover:text-rose-pink font-medium text-xl"
+        >
+          Home
+        </button>
+        <button
+          onClick={() => navigate("/favourites")}
+          className="text-dark-pink transition-all duration-300 hover:text-rose-pink font-medium text-xl"
+        >
+          Favourites
+        </button>
+      </div>
+      <div className="flex items-center md:space-x-6">
+        <span className="text-dark-pink font-medium hidden md:inline text-xl">
+          Thomas Shelby
+        </span>
+        <span
+          onClick={() => navigate("/logout")}
+          className="material-symbols-outlined text-dark-pink transition-all duration-300 hover:text-rose-pink text-2xl cursor-pointer"
+        >
+          logout
+        </span>
+      </div>
+    </nav>
+  );
+}
