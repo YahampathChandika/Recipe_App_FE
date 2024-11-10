1b9d3543d4e43ef41cc42ca34411edfcb829939f
Yahampath Chandika <yhmpth@gmail.com>
2024-11-10 11:57:05 +0530

    no data

--- src/pages/Favourites.jsx
+++ src/pages/Favourites.jsx
@@ -6,6 +6,7 @@ import {
 } from "../store/api/favouriteApi";
 import { CircularProgress } from "@mui/material";
 import Swal from "sweetalert2";
+import nodata from "../assets/images/nofav.svg";
 
 export default function Favourites() {
   const {
@@ -15,6 +16,7 @@ export default function Favourites() {
     refetch,
   } = useGetFavouriteRecipesQuery();
   const [removeFavourites] = useRemoveFavouriteRecipeMutation();
+
   useEffect(() => {
     document.title = "Favourites | Cook";
   }, []);
@@ -25,7 +27,6 @@ export default function Favourites() {
       await refetch();
 
       if (response.error) {
-        console.log("error");
         Swal.fire({
           title: "Oops...",
           text: response?.error?.data?.message,
@@ -64,9 +65,20 @@ export default function Favourites() {
         <div className="flex justify-center items-center text-red-500 min-h-[500px] md:text-lg">
           Error loading recipes. Please try again later.
         </div>
+      ) : recipes?.length === 0 ? (
+        <div className="flex flex-col justify-center items-center h-full text-center pt-24 md:pt-48 px-4">
+          <img
+            src={nodata}
+            alt="No favourites"
+            className="w-24 md:w-48 mb-10"
+          />
+          <p className="text-rose-pink text-lg md:text-2xl">
+            No favourites yet. Start adding your favorite recipes!
+          </p>
+        </div>
       ) : (
         <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-8 px-4 md:px-24 py-8 md:py-16">
-          {recipes?.map((recipe) => (
+          {recipes.map((recipe) => (
             <div
               key={recipe._id}
               className="bg-white rounded-lg shadow-md overflow-hidden hover:shadow-xl transition cursor-pointer"
@@ -86,7 +98,7 @@ export default function Favourites() {
                     }}
                     className="mt-2 text-rose-pink hover:text-dark-pink"
                   >
-                    <i class="fas fa-heart text-xl"></i>
+                    <i className="fas fa-heart text-xl"></i>
                   </button>
                 </div>
                 <h3 className="text-lg font-semibold text-dark-pink">
